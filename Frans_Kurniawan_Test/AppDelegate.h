4bf7b3cdd774dfb4f1ca8d5f90506835b56fcc9e
Qisha Sadiya Kendy <qsk@Qishas-MacBook-Pro-2.local>
2014-09-29 11:17:21 -0400

    Initial Commit

---
+++ Frans_Kurniawan_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Frans_Kurniawan_Test
+//
+//  Created by Qisha Sadiya Kendy on 9/29/14.
+//  Copyright (c) 2014 FransKurniawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Frans_Kurniawan_Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Frans_Kurniawan_Test
+//
+//  Created by Qisha Sadiya Kendy on 9/29/14.
+//  Copyright (c) 2014 FransKurniawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
